abd58e508c6174abd8f54f05f034f3b4496e71df
TeoGoerens <goerens_teo@hotmail.com>
2018-12-01 14:06:56 -0300

    Edicion de perfil de usuario casi terminada

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -72,7 +72,46 @@ class UserController extends Controller
      */
     public function update(Request $request)
     {
-        dd($request);
+        $rules = [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'genre' => 'required',
+            'birth_date' => 'required',
+            'phone_number' => 'required|integer',
+            'country' => 'required',
+            'city' => 'required',
+            'instrument' => 'required',
+            'band_name' => 'required'
+        ];
+
+        $messages = [
+            'first_name.required' => 'El campo nombre es obligatorio',
+            'last_name.required' => 'El campo apellido es obligatorio',
+            'genre.required' => 'El campo sexo es obligatorio',
+            'birth_date.required' => 'El campo fecha de nacimiento es obligatorio',
+            'phone_number.required' => 'El campo número de teléfono es obligatorio',
+            'phone_number.integer' => 'El campo número de teléfono debe ser numérico',
+            'country.required' => 'El campo país es obligatorio',
+            'city.required' => 'El campo ciudad es obligatorio',
+            'instrument.required' => 'El campo instrumento es obligatorio',
+            'band_name.required' => 'El campo banda es obligatorio'
+        ];
+
+        $validator = $this->validate($request, $rules, $messages);
+        $newUser = Auth::user();
+
+        $newUser->first_name = $validator['first_name'];
+        $newUser->last_name = $validator['last_name'];
+        $newUser->genre = $validator['genre'];
+        $newUser->birth_date = $validator['birth_date'];
+        $newUser->phone_number = $validator['phone_number'];
+        $newUser->country = $validator['country'];
+        $newUser->city = $validator['city'];
+        $newUser->instrument = $validator['instrument'];
+        $newUser->band_name = $validator['band_name'];
+
+        $newUser->save();
+
         return view('perfil.show')->with('user', Auth::user());
     }
 
