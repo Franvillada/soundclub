b98c6d02c341c0a9d1036472d2ad18392c85d390
TeoGoerens <goerens_teo@hotmail.com>
2018-12-11 21:58:16 -0300

    Cambios en el perfil de usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -48,8 +48,9 @@ class UserController extends Controller
      */
     public function show($name)
     {
-        $user = User::where('name','=',$name);
-        dd($user);
+        $queryUser = User::where('name', '=', $name)->get();
+        $user = $queryUser[0];                                      //Esto debe hacerse dado que el query de SQL nos trae un $queryUser como una colección de resultados porque podría haber más de uno. En este caso, como hago la búsqueda a través del nombre de usuario que es único, entonces el subíndice 0 me trae el resultado correcto
+        return view('perfil.show')->with('user', $user);
     }
 
     /**
